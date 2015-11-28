ea4c44d67e8cf51654b2d71ca5f8c238905e80f5
julien-monniot <nceska@f813a028846a>
2015-11-28 21:06:09 +0000

    Almost ready for prod on python anywhere

--- wup/settings.py
+++ wup/settings.py
@@ -102,8 +102,12 @@ USE_TZ = False  # that was set to True at project creation
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = 'static'
+
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+   # os.path.join(BASE_DIR, "static"),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "medias")
@@ -112,22 +116,3 @@ MEDIA_URL = '/media/'
 LOGIN_URL = '/users/login/'
 
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'static'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-            os.path.join(BASE_DIR, 'static'),
-            )
