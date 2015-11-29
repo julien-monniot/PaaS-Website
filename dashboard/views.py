c05ee01452e65a239a46656e0083558d5d7da199
julien-monniot <n.ceska@gmail.com>
2015-11-29 22:24:39 +0100

    Linking between the different pages

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def main_panel(request):
-    html = "<html><body>Main dashboard where users will get access to any website functionality" \
-           "<br><a href='/users/logout/'>Logout</a></body></html>"
-    return HttpResponse(html)
+    return render(request, 'dashboard/index.html')
