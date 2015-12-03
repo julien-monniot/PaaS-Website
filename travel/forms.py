51426c790e2c5f8e24422ff97866e0a6be0ac856
julien-monniot <n.ceska@gmail.com>
2015-12-03 15:57:33 +0100

    Form template in progress

--- travel/forms.py
+++ travel/forms.py
@@ -5,27 +5,24 @@ from django.core.validators import MaxValueValidator
 
 class TravelForm(forms.ModelForm):
     title = forms.CharField(max_length=200, help_text="Le titre de votre voyage")
-    description = forms.TextField(help_text="Description du voyage")
-    starting_date = forms.DateField(blank=True, null=True)
-    ending_date = forms.DateField(blank=True, null=True)
-    image = forms.ImageField(upload_to='images', default='images/road-trip.jpg')
-    budget = forms.PositiveSmallIntegerField(blank=True, help_text='Bugdet prévisionnel par personne pour le voyage entier',
-                                              validators=[MaxValueValidator(limit_value=1000)])
+    description = forms.CharField(max_length=1000, help_text="Description du voyage")
+    starting_date = forms.DateField()
+    ending_date = forms.DateField()
+    image = forms.ImageField()
+    budget = forms.IntegerField(help_text='Bugdet prévisionnel par personne pour le voyage entier', min_value=0, max_value=1500)
 
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    fields = ('title', 'description', 'starting_date', 'ending_date', 'budget', 'image')
 
     class Meta:
         model = Travel
-
+        fields = ('title', 'description', 'starting_date', 'ending_date', 'budget', 'image')
 
 class StageForm(forms.ModelForm):
     title = forms.CharField(max_length=200)
     point_of_departure = forms.CharField(max_length=300)
     point_of_arrival = forms.CharField(max_length=300)
-    duration = forms.PositiveSmallIntegerField(default=1, blank=True, help_text='Durée en jours pour l\'étape',
-                                                validators=[MaxValueValidator(limit_value=21)])
-    description = forms.TextField(blank=True)
+    duration = forms.IntegerField(help_text='Durée en jours pour l\'étape', max_value=21, min_value=0)
+    description = forms.CharField(max_length=1000)
 
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
--- travel/views.py
+++ travel/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Travel
+from .forms import TravelForm
 
 
 @login_required
@@ -8,12 +9,28 @@ def travel_list(request):
     travels = Travel.objects.order_by('created_date')
     return render(request, 'travel/travel_list.html', {'travels': travels})
 
+
 @login_required
 def travel_detail(request, pk):
     travel = get_object_or_404(Travel, pk=pk)
     return render(request, 'travel/travel_detail.html', {'travel': travel})
 
+
 @login_required
 def create_travel(request):
-    html = "<html><body>Here there will be a form used to create a new travel</body></html>"
-    return HttpResponse(html)
+    if request.method == 'POST':
+        form = TravelForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            #return travel_detail(request, )
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print(form.errors)
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = TravelForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render('travel/add_travel.html', {'form': form})
