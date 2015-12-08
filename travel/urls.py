1c2abceace6f023efd3f8b3498b41b8bdf4b57ea
Toumassian Arthur <artogu@live.fr>
2015-12-09 00:42:34 +0100

    Design++
    Logo + user information in the menu
    "Mes voyages" --> OK
    "Voyages que j'ai créés" --> OK

--- travel/urls.py
+++ travel/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.travel_list, name='travel_list'),
     url(r'^(?P<pk>\d+)/$', views.travel_detail, name='travel_detail'),
-    url(r'^create/$', views.create_travel, name='create_travel'),
+    url(r'^mytravels/$', views.my_travels, name='my_travels'),
+    url(r'^mycreatedtravels/$', views.my_created_travels, name='my_created_travels'),
+    url(r'^create/$', views.create_travel, name='create_travel')
 )
--- travel/views.py
+++ travel/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Travel
 from .models import Stage
+from .models import Participate
 
 @login_required
 def travel_list(request):
     travels = Travel.objects.order_by('created_date')
-    return render(request, 'travel/travel_list.html', {'travels': travels})
+    return render(request, 'travel/travel_list.html', {'title':'Liste des voyages', 'travels': travels})
+
+@login_required
+def my_created_travels(request):
+	travels = Travel.objects.filter(author__user=request.user.id).order_by('created_date')
+	return render(request, 'travel/travel_list.html', {'title':'Mes voyages créés', 'travels': travels})
+
+@login_required
+def my_travels(request):
+    travels = Travel.objects.filter(id__in=[participate.travel.id for participate in Participate.objects.filter(person__user=request.user.id)]).order_by('created_date')
+    return render(request, 'travel/travel_list.html', {'title':'Mes voyages', 'travels': travels})
 
 @login_required
 def travel_detail(request, pk):
