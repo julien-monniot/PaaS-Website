c2a0fddefef9575fe509d609a61025a258c2ca76
Toumassian Arthur <artogu@live.fr>
2015-12-10 22:17:06 +0100

    User can subscribe or unsubscribe to a travel

--- travel/urls.py
+++ travel/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.travel_list, name='travel_list'),
     url(r'^(?P<pk>\d+)/$', views.travel_detail, name='travel_detail'),
+
+    url(r'^(?P<pk>\d+)/subscribe$', views.travel_subscribe, name='travel_subscribe'),
+    url(r'^(?P<pk>\d+)/unsubscribe$', views.travel_unsubscribe, name='travel_unsubscribe'),
     url(r'^mytravels/$', views.my_travels, name='my_travels'),
     url(r'^mycreatedtravels/$', views.my_created_travels, name='my_created_travels'),
     url(r'^create/$', views.create_travel, name='create_travel')
--- travel/views.py
+++ travel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from .models import Travel
 from .models import Stage
 from .models import Participate
@@ -20,11 +21,35 @@ def my_travels(request):
     return render(request, 'travel/travel_list.html', {'title':'Mes voyages', 'travels': travels})
 
 @login_required
-def travel_detail(request, pk):
+def travel_detail(request, pk, message=""):
     travel = get_object_or_404(Travel, pk=pk)
     stages = Stage.objects.filter(travel=travel.id)
     participants = travel.participants.all()
-    return render(request, 'travel/travel_detail.html', {'travel': travel, 'stages':stages, 'participants':participants})
+    participating = False
+    if len(travel.participants.filter(user=request.user.id)) > 0:
+        participating = True
+    return render(request, 'travel/travel_detail.html', {'message':message, 'travel': travel, 'stages':stages, 'participants':participants, 'participating':participating})
+
+@login_required
+def travel_subscribe(request, pk):
+    travel = get_object_or_404(Travel, pk=pk)
+    if len(travel.participants.filter(user_id=request.user.id)) < 1:
+        participation=Participate(person=request.user.wuprofil, travel=travel, motivation=5)
+        participation.save()
+
+    return HttpResponseRedirect('/travel/%s/' % pk)
+
+
+@login_required
+def travel_unsubscribe(request, pk):
+    travel = get_object_or_404(Travel, pk=pk)
+    participations=Participate.objects.filter(person__user__id=request.user.id, travel_id=pk)
+    if len(participations) > 0:
+        for participation in participations:
+            participation.delete()
+     
+
+    return HttpResponseRedirect('/travel/%s/' % pk)
 
 @login_required
 def create_travel(request):
