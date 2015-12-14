f03233f94bb33e6012ad7bbc215655a99cfe19b9
Toumassian Arthur <artogu@live.fr>
2015-12-14 17:35:40 +0100

    Notification.html

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from .models import Historique, Flow, Notification
@@ -8,6 +8,24 @@ from .models import Historique, Flow, Notification
 def main_panel(request):
     return render(request, 'dashboard/dashboard.html', 
 		{
-			"historique":Flow.objects.filter(Q(receiver__user__id=request.user.id)| Q(receiver__isnull=True)).order_by('-fact__date')
+			"title":"Bienvenue","historique":Flow.objects.filter(Q(receiver__user__id=request.user.id)| Q(receiver__isnull=True)).order_by('-fact__date')
+		}
+	)
+@login_required
+def notifications(request):
+	return render(request, 'dashboard/notifications.html', 
+		{
+			"title":"Vos Notifications","notifications":Notification.objects.filter(receiver__user__id=request.user.id)
+		}
+	)
+
+@login_required
+def notification_link(request, pk):
+	notif = get_object_or_404(Notification, pk=pk)
+	notif.seen = True
+	notif.save()
+	return render(request, 'dashboard/dashboard.html', 
+		{
+			"historique":Flow.objects.filter(fact__id=notif.fact.id)
 		}
 	)
